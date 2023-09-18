16e8177d4a8f2541fd06c8aaef2f64e25d67adca
Meghana Gundeboina <124271236+meghana010@users.noreply.github.com>
2023-09-18 07:49:35 +0530

    Update ShopCart.jsx

--- myapp/src/components/shops/ShopCart.jsx
+++ myapp/src/components/shops/ShopCart.jsx
@@ -2,10 +2,7 @@
 import React, { useState } from "react"
 
 const ShopCart = ({ shopItems, addToCart }) => {
-  const [count, setCount] = useState(0)
-  const increment = () => {
-    setCount(count + 1)
-  }
+  
 
   return (
     <>
